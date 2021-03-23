bf446456c97b5d7d51daff4f9813768b924cdaad
LadyzRoman <ladyz1roman@gmail.com>
2021-03-23 16:46:35 +0300

    model improvement

--- client/recipe-book/src/app/model/recipe.ts
+++ client/recipe-book/src/app/model/recipe.ts
@@ -2,11 +2,14 @@ import {Step} from './step';
 import {Ingredient} from './ingredient';
 
 export interface Recipe {
-    id: number;
-    title: string;
-    cookingTime: number;
-    steps: Step[];
-    ingredients: Ingredient[];
-    imgId?: string;
-    description?: string;
+  id: number;
+  title: string;
+  cookingTime: number;
+  steps: Step[];
+  ingredients: Ingredient[];
+  createdAt: Date;
+  imgId?: string;
+  description?: string;
+  category: string;
+  tags: string[];
 }
