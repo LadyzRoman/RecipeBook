d397dc867db8ff74f1aa85743a9376f29ffd4b32
LadyzRoman <ladyz1roman@gmail.com>
2021-03-24 11:54:45 +0300

    updated mock data

--- client/recipe-book/src/app/mock/recipes.ts
+++ client/recipe-book/src/app/mock/recipes.ts
@@ -1,12 +1,34 @@
 import {Recipe} from '../model/recipe';
 
 export const RECIPES: Recipe[] = [
-  {id: 1, title: 'Котлетка с пюрешкой', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: []},
-  {id: 2, title: 'Жареная курица', cookingTime: 60, steps: [], ingredients: [], imgId: '1', createdAt: new Date(), category: '1', tags: []},
-  {id: 3, title: 'Салат', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: []},
-  {id: 4, title: 'Борщ', cookingTime: 60, steps: [], ingredients: [], imgId: '2', createdAt: new Date(), category: '1', tags: []},
-  {id: 5, title: 'Солянка', cookingTime: 60, steps: [], ingredients: [], imgId: '5', createdAt: new Date(), category: '1', tags: []},
-  {id: 6, title: 'Плов', cookingTime: 60, steps: [], ingredients: [], imgId: '4', createdAt: new Date(), category: '1', tags: []},
-  {id: 7, title: 'Шаверма', cookingTime: 60, steps: [], ingredients: [], imgId: '3', createdAt: new Date(), category: '1', tags: []},
-  {id: 8, title: 'Спагетти болольезе', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: []}
+  {
+    id: 1, title: 'Котлетка с пюрешкой', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: '9e922844-3ebf-455f-9fad-6e1a7a8b53fd',
+  },
+  {
+    id: 2, title: 'Жареная курица', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: '59f46033-19c2-4e0b-88fc-0d46d9efb0d3',
+  },
+  {
+    id: 3, title: 'Салат', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+  },
+  {
+    id: 4, title: 'Борщ', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: '2df30a29-c87b-4b27-9a46-f7c6fad32117',
+  },
+  {
+    id: 5, title: 'Солянка', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: 'f17ef797-8ab1-479e-9b8b-2fbee87491df',
+  },
+  {
+    id: 6, title: 'Плов', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: '786ffed7-2150-4520-95f4-91da4c2453ab',
+  },
+  {
+    id: 7, title: 'Шаверма', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+    imgId: '0db1261f-0834-4283-b574-213a2e075ca5',
+  },
+  {
+    id: 8, title: 'Спагетти болольезе', cookingTime: 60, steps: [], ingredients: [], createdAt: new Date(), category: '1', tags: [],
+  },
 ];
