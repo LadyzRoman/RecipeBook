3bdbc072c99bea097aa8d00f1497d97a2947ad45
AlekssGit <alvasin98@gmail.com>
2021-05-01 20:23:50 +0300

    adding new features to page /recipe-add

--- client/recipe-book/src/app/components/recipe-adding/recipe-adding.component.ts
+++ client/recipe-book/src/app/components/recipe-adding/recipe-adding.component.ts
@@ -2,13 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../../model/recipe';
 import {RecipeService} from '../../services/recipe/recipe.service';
 import {first} from 'rxjs/operators';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Location} from '@angular/common';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Step} from '../../model/step';
 import {Ingredient, Unit} from '../../model/ingredient';
 import {Category} from '../../model/category';
 import {of} from 'rxjs';
+import {CategoryService} from '../../services/category/category.service';
 
 @Component({
   selector: 'app-recipe-adding',
@@ -19,32 +20,44 @@ export class RecipeAddingComponent implements OnInit {
 
   // @ts-ignore
   formIngredient: FormGroup;
+  // @ts-ignore
+  formStep: FormGroup;
 // @ts-ignore
   formRecipe: FormGroup;
   loading = false;
   submitted = false;
   // @ts-ignore
   ingredients: Ingredient[];
+  // @ts-ignore
+  steps: Step[];
 // @ts-ignore
   formUnits: FormGroup;
   units = [] as any;
+
+
+  categories: Category[] = [];
+
+  form = new FormGroup({
+    category: new FormControl()
+  });
 
   constructor(
     private location: Location,
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private categoryService: CategoryService
   ) {
-
-    this.formUnits = this.formBuilder.group({
-      units: ['']
-    });
-    of(this.getUntis()).subscribe(units => {
-      this.units = units;
-      this.formUnits.controls.units.patchValue(this.units[0].id);
-    });
+      this.categoryService.getCategories().subscribe(categories => {
+        this.categories = [];
+        this.categories.push({id: 'undefined', title: 'Любая категория'});
+        this.categories = this.categories.concat(categories);
+        this.form.patchValue({category: this.categories[0]});
+      });
   }
+
+
 
 
   /*  id: number;
@@ -61,10 +74,14 @@ export class RecipeAddingComponent implements OnInit {
   ngOnInit(): void {
     // Не нравится это дело, но без такой инициализации не работает
     this.ingredients = new Array(0);
+    this.steps = new Array(0);
 
     this.formRecipe = this.formBuilder.group({
       title: ['', Validators.required],
-      cookingTime: ['', Validators.required]
+      Img: ['', Validators.required],
+      description: ['', Validators.required],
+      cookingTime: ['', Validators.required],
+      category: ['', Validators.required]
     });
 
     this.formIngredient = this.formBuilder.group({
@@ -73,14 +90,33 @@ export class RecipeAddingComponent implements OnInit {
       unit: ['', Validators.required]
     });
 
+    this.formStep = this.formBuilder.group({
+      description: ['', Validators.required],
+      img: ['', Validators.required]
+    });
 
+    this.formUnits = this.formBuilder.group({
+      units: ['']
+    });
+    of(this.getUntis()).subscribe(units => {
+      this.units = units;
+      this.formUnits.controls.units.patchValue(this.units[0].id);
+    });
   }
 
   get f(): { [p: string]: AbstractControl } {
     return this.formRecipe.controls;
   }
 
+  get f_ingredient(): { [p: string]: AbstractControl } {
+    return this.formIngredient.controls;
+  }
+
   addIngredient(name: string, count: number, strUnit: string): void {
+    this.submitted = true;
+    if (this.formIngredient.invalid) {
+      return;
+    }
     const unit = parseInt(strUnit, 10);
     const id = this.ingredients.length + 1;
     if (name.length > 1) {
@@ -99,9 +135,13 @@ export class RecipeAddingComponent implements OnInit {
     ];
   }
 
+  addStep(text: string, imgId: string): void {
+    const id = this.ingredients.length + 1;
+    this.steps.push({id, text, imgId} as Step);
+  }
+
   onSubmit(): void {
     this.submitted = true;
-
     if (this.formRecipe.invalid) {
       return;
     }
@@ -116,5 +156,13 @@ export class RecipeAddingComponent implements OnInit {
         error => {
           this.loading = false;
         });
+  }
+
+  deleteIngredient(i: number): void {
+    this.ingredients.splice(i, 1);
+  }
+
+  deleteStep(i: number): void {
+    this.steps.splice(i, 1);
   }
 }
